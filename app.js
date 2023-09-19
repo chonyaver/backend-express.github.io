2019e81c12c0750babb7130454d67bb923f2d0f9
chonyaver <fs.chonyaver@gmail.com>
2023-09-19 21:37:46 +0700

    Add 4 files via upload
    .env, app.js, app.json, package.json

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const newOrderRouter = require('./routes/new-order');
+const currenciesRouter = require('./routes/currencies');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/new-order', newOrderRouter);
+app.use('/currencies', currenciesRouter);
+
+module.exports = app;
